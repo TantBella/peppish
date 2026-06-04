9c7c3725868d2f8cc1ab001db58e73c6713e3d57
therealtantB <111968724+TantBella@users.noreply.github.com>
2026-06-04 08:47:03 +0200

    auto-run migration on startup to ensure db schema is up to date (#90)

--- API_Peppish/API_Peppish/Program.cs
+++ API_Peppish/API_Peppish/Program.cs
@@ -121,4 +121,10 @@ app.UseAuthorization();
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+  var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+  db.Database.Migrate();
+}
+
 app.Run();
