51bd4da1d8a3382b3e8420bccffd41af5633e3b1
Bella Solomon-Falkfrii - CDA24 GBG <bella.solomon-falkfrii@iths.se>
2026-09-07 10:32:29 +0200

    added fe-localhost

--- API_Peppish/API_Peppish/Program.cs
+++ API_Peppish/API_Peppish/Program.cs
@@ -126,7 +126,7 @@ builder.Services.AddCors(options =>
   options.AddPolicy("AllowFrontend", policy =>
   {
     policy
-          .WithOrigins("https://peppish.vercel.app", "http://localhost:3000", "http://localhost:5000")
+          .WithOrigins("https://peppish.vercel.app", "http://localhost:3000", "http://localhost:5000", "http://localhost:5173")
           .AllowAnyHeader()
           .AllowAnyMethod();
   });
