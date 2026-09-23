a52ceb158c0c8b9b6fd23c5c7d9441b72133d2d0
therealtantB <111968724+TantBella@users.noreply.github.com>
2026-09-23 11:47:43 +0200

    Quest notification (#248)
    * fix notification for quests and only show notification about householdreqs to adults
    
    * wip

--- API_Peppish/API_Peppish/Services/ChoreAssignmentService.cs
+++ API_Peppish/API_Peppish/Services/ChoreAssignmentService.cs
@@ -116,15 +116,9 @@ namespace API_Peppish.Services
             {
                 try
                 {
-                    var payload =
-                        System.Text.Json.JsonSerializer.Serialize(
-                            new
-                            {
-                                assignmentId = assignment.Id,
-                                templateId = assignment.ChoreTemplateId,
-                                startDate = assignment.StartDate,
-                                dueDate = assignment.DueDate
-                            });
+                    var payload = template.RewardType == RewardType.Money
+                     ? $"Du har fått en ny quest: {template.Title}. Dags att kamma hem lite cash!"
+                     : $"Du har fått en ny quest: {template.Title}. Dags att håva in din XP!";
 
                     await notificationService.CreateNotificationAsync(
                         new CreateNotificationRequest
